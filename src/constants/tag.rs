c355f1379b90465a42e667729451b8c7e3e3651a
Jonathan Oesterberg <jonte_ost@live.com>
2024-07-29 21:59:14 +0000

    Add: 'scan_for_bot' and improve tag/kind const.

--- src/constants/tag.rs
+++ src/constants/tag.rs
@@ -1,8 +1,9 @@
+pub const BOT: &str = "Bot";
 pub const COMPONENT: &str = "BotComponent";
 pub const MINE: &str = "Mine";
 pub const OIL: &str = "Oil";
+pub const PLASMA_SHIELD: &str = "PlasmaShield";
+pub const ROCK: &str = "Rock";
 pub const SENTRY: &str = "Sentry";
 pub const SHIELD: &str = "Shield";
 pub const WALL: &str = "Wall";
-pub const ROCK: &str = "Rock";
-pub const PLASMA_SHIELD: &str = "PlasmaShield";
